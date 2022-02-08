b7872617b17949521806831ae8cb542bab2ecdb8
navarrofeliza <navarrofeliza@yahoo.com>
2022-02-08 08:50:59 -0800

    boxing/unboxing submission

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var boxed = new List<object>();
+
+        boxed.Add(7);
+        boxed.Add(28);
+        boxed.Add(-1);
+        boxed.Add(true);
+        boxed.Add("chair");
+
+        int sum = 0;
+        foreach(var item in boxed)
+        {
+            Console.WriteLine(item);
+            if(item is int)
+            {
+                Console.WriteLine("Looks like we have an int!");
+                sum += (int)item;
+                Console.WriteLine($"Current sum: {sum}");
+            }
+        }
+    }
+}
